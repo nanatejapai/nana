5173c5ba1df8ff8167e07dee4621d79607d44118
nanatejapai <Nana@natvipas-mbp-2.risd.edu>
2017-04-20 03:28:10 -0400

    changeeee
    d

--- counting time/time.js
+++ counting time/time.js
@@ -1,5 +1,5 @@
 
-console.log("hello from external script.js");
+// console.log("hello from external script.js");
 
 $(document).ready(function() {
   clockUpdate();
@@ -33,80 +33,4 @@ function clockUpdate() {
 
   $('.digital-clock').text(h + ':' + m + ':' + s)
 }
-
-// var audioElement = document.createElement('audio');
-//     audioElement.setAttribute('src', 'effect.mp3');
-    
-//     audioElement.addEventListener('ended', function() {
-//         this.play();
-//     }, false); audioElement.addEventListener("timeupdate",function(){
-//         $("#currentTime").text("Current second:" + audioElement.currentTime);
-//     });   
-
-
-
-
-// var siID;
-// $(document).ready(function(){
-//         $('#startTimer').click(function(){
-//         var sec = 0;
-//         siID = setInterval(function() {
-//         $('#counter span').text(++sec/10000);}, 27); 
-//         })
-        
-//         $('#stopTimer').click(function() {
-//             window.clearInterval(siID);
-//             $('#counter span').text('0');
-//         });
-// });
-
-
-// $('#stopTimer').click(function(){
-// 	audioElement.pause();
-// 	});
-// $('#startTimer').click(function(){
-// 	audioElement.play();
-// 	});
-// $('#startTimer').click(function(){
-//     $(".result").show();
-// });
-// $('#stopTimer').click(function(){
-//     $(".result").hide();
-// });
-// // $('.hide').click(function() {
-// //         $('.counter').hide();audioElement.pause();
-// //     });
-
-
-// // $( '.show' ).click(function() {
-// //   $( this ).replaceWith( '.hide');
-// // });
-
-// // $('.button').click(function(){
-// //     var $this = $(this);
-// //     $this.toggleClass('button');
-// //     if($this.hasClass('button')){
-// //       $this.text('show');     
-// //     } else {
-// //       $this.text('hide');
-// //     }
-// //   });
-
-// // function startCounter(){
-// // 	$('.counter').each(function (index) {
-// // 	    $(this).prop('Counter',0).animate({
-// // 	        Counter: $(this).text()
-// // 	    }, {
-// // 	        duration: 1800000,
-// // 	        easing: 'swing',
-// // 	        step: function (now) {
-// // 	            $(this).text(Math.round(now*10)/100000);
-// // 	        }
-// // 	    });
-// // 	});
-// // }	
-
-// // startCounter();
-// //    $('')
-// });
 
